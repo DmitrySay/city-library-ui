89ee4fd930a960cad3b116a552758fa66ad076b8
dmitry.sayushev <dmitry_sayushev@solbeg.soft>
2021-10-01 12:33:58 +0300

    add env dev/prod

--- src/constants/HttpConfig.js
+++ src/constants/HttpConfig.js
@@ -1,6 +1,5 @@
 export const HttpConfig = {
     domains: {
-        api:
-            process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api' : process.env.REACT_APP_API_HOST,
-    },
+        api: `${process.env.REACT_APP_API_HOST}/api`
+    }
 };
