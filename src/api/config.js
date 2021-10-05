4587d745c3897a1b2b39b5c5208aafb628c4b1a1
dmitry.sayushev <dmitry_sayushev@solbeg.soft>
2021-10-05 17:02:54 +0300

    ready for demo v1.0

--- src/api/config.js
+++ src/api/config.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const instantiateConfig = () => {
- // console.log("window._env_.REACT_APP_API_HOST", `${window._env_.REACT_APP_API_HOST}/api/`);
-  // axios.defaults.baseURL = `${window._env_.REACT_APP_API_HOST}/api/`;
-  axios.defaults.baseURL = 'http://localhost:8080/api';
+    axios.defaults.baseURL = `${process.env.REACT_APP_API_HOST}/api`
 };
 
 export default instantiateConfig;
